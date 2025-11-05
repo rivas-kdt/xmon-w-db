a0913e677edc63e4ccc2fb29f62983764f1e0ea2
tanedo-kdt <tanedo-kdt@global.kawasaki.com>
2025-11-05 12:19:57 +0800

    new loader

--- src/features/auth/components/protectedRoute.tsx
+++ src/features/auth/components/protectedRoute.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useSession } from "../hooks/sessionProvider";
+import { useTheme } from "next-themes";
 
 export default function ProtectedRoute({
   children,
@@ -11,16 +12,36 @@ export default function ProtectedRoute({
 }) {
   const { user, loading } = useSession();
   const router = useRouter();
+  const { theme } = useTheme();
 
   useEffect(() => {
     if (!loading && !user) {
       router.replace("/login");
     }
   }, [user, loading, router]);
+  console.log({ user, loading });
 
-  if (loading) return <div className="p-4 text-center">Loading...</div>;
+  if (loading)
+    return (
+      <div
+        className={`flex items-center justify-center w-full h-screen bg-background transition-colors duration-300 ${
+          theme === "dark" ? "bg-[#0f1729]" : "bg-[#f8fafc]"
+        }`}
+      >
+        <div className="loader">
+          {[...Array(8)].map((_, i) => (
+            <div key={i} className={`box box${i}`}>
+              <div></div>
+            </div>
+          ))}
+          <div className="ground">
+            <div></div>
+          </div>
+        </div>
+      </div>
+    );
 
   if (!user) return null;
 
-  return <>{children}</>;
+  return children;
 }
